96d45129e4772b4f421e094125838f102b823dbc
Aman <a.shalginbayev@yandex.com>
2026-04-24 13:12:59 +0500

    Add: concatenation-of-array - submission-6

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-6.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int[] GetConcatenation(int[] nums) {
+        var resultNums = new List<int>();
+
+        for (var i = 0; i < 2; i++) {
+            foreach (var n in nums) {
+                resultNums.Add(n);
+            }
+        }
+
+        return resultNums.ToArray();
+    }
+}
